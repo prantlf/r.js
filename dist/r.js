50377d7dfe12ab9106a22388f349da48bc2d6c18
Ferdinand Prantl <prantlf@gmail.com>
2022-02-13 11:40:02 +0100

    chore: Update the distribution

--- dist/r.js
+++ dist/r.js
@@ -1,5 +1,5 @@
 /**
- * @license r.js 2.4.7 Sun, 13 Feb 2022 09:24:56 GMT Copyright jQuery Foundation and other contributors.
+ * @license r.js 2.4.8 Sun, 13 Feb 2022 10:38:44 GMT Copyright jQuery Foundation and other contributors.
  * Released under MIT license, http://github.com/requirejs/r.js/LICENSE
  */
 
@@ -19,7 +19,7 @@ var requirejs, require, define, xpcUtil;
 (function (console, args, readFileFunc) {
     var fileName, env, fs, vm, path, exec, rhinoContext, dir, nodeRequire,
         nodeDefine, exists, reqMain, loadedOptimizedLib, existsForNode, Cc, Ci,
-        version = '2.4.7 Sun, 13 Feb 2022 09:24:56 GMT',
+        version = '2.4.8 Sun, 13 Feb 2022 10:38:44 GMT',
         jsSuffixRegExp = /\.js$/,
         commandOption = '',
         useLibLoaded = {},
@@ -28886,11 +28886,13 @@ define('build', function (require) {
                         }
                     };
                     if (mapping.source != null) {
-                        newMapping.source = mapping.source;
-                        newMapping.original = {
-                            line: mapping.originalLine,
-                            column: mapping.originalColumn
-                        };
+                        if (mapping.originalLine != null) {
+                            newMapping.source = mapping.source;
+                            newMapping.original = {
+                                line: mapping.originalLine,
+                                column: mapping.originalColumn
+                            };
+                        }
                         if (mapping.name != null) {
                             newMapping.name = mapping.name;
                         }
